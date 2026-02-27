9e743c0764e3a46d405e92d8da9562f32a299b5c
Kiernan Jennings <kiernan.xj@gmail.com>
2026-02-27 12:51:44 -0600

    intialize project

---
+++ src/formulation.jl
@@ -0,0 +1,13 @@
+using JuMP, Ipopt, HiGHS, Gurobi
+
+function monolithic_formulation()
+    
+end
+
+function NLP_formulation()
+    
+end
+
+function MILP_formulation()
+    
+end
---
+++ src/main.jl
@@ -0,0 +1,2 @@
+include("formulation.jl")
+include("utils.jl")
