3996aac877f2e8c9e3a0733acbbdbfcf53fec810
Kiernan Jennings <kiernan.xj@gmail.com>
2026-03-12 15:45:39 -0500

    Start brute force data gathering

--- src/formulation.jl
+++ src/formulation.jl
@@ -1,5 +1,5 @@
 # Optimization Problem Structure
-using JuMP, DiffOpt, Ipopt
+using JuMP, DiffOpt, Ipopt, Gurobi
 
 # Surrogate Model Architecture
 using Flux
@@ -7,3 +7,31 @@ using Flux
 """
 Here is where we will maintain the problem structure and the core functions to running this problem.
 """
+
+
+struct TrainingData
+    u_bar::Vector{Float64}
+    x_star::Vector{Float64}
+end  
+
+
+function formulate_monolithic()
+    """
+    Function to formulate the MIQCQP problem.
+    """
+end
+
+
+function solve_monolithic(model::JuMP.Model)
+    """
+    Function to gather training data for the convex MIQCQP.
+        model - JuMP model containing the monolithic MIQCP
+    Returns:
+        training_data - mapping of the fixed parameters \bar{u} --> x*
+    """
+    optimize!(model)
+    
+    u = value.(model[u])
+    
+    return 
+end
--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,14 @@
 include("formulation.jl")
 include("utils.jl")
 
+
+
 """
 Here is where we will run the case study.
+
+
+Case study: Unit commitment with AC OPF 
+
+Using convex QC approximation for ACOPF
+https://link.springer.com/article/10.1007/s11081-026-10079-4?utm_source=rct_congratemailt&utm_medium=email&utm_campaign=oa_20260311&utm_content=10.1007/s11081-026-10079-4
 """
